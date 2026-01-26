4879455de8f9d1f60b519078365650adb4295721
brodievilleneuve <brodievilleneuve@gmail.com>
2026-01-26 21:44:30 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,29 +1,21 @@
 { pkgs, ... }: {
-  # Nix channel, determines available package versions
   channel = "stable-24.05";
 
-  # Packages to install
   packages = [
-    pkgs.nodejs_20  # For Node.js runtime
-    pkgs.nodePackages.typescript # For TypeScript support
-    pkgs.nodePackages.typescript-language-server
+    pkgs.nodejs_20
   ];
 
-  # VS Code extensions to install
   idx = {
     extensions = [
-      "dbaeumer.vscode-eslint",
+      "dbaeumer.vscode-eslint"
       "vscodevim.vim"
     ];
 
-    # Workspace lifecycle hooks
     workspace = {
-      # Runs when a workspace is first created
       onCreate = {
         root-npm-install = "npm install";
         functions-npm-install = "npm --prefix functions install";
       };
-      # Runs every time the workspace is (re)started
       onStart = {};
     };
   };
